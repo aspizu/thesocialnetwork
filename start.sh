761d8c966b09b730ec26c44cf9b5aca160c63100
aspizu <aspizu@protonmail.com>
2025-08-14 00:40:57 +0530

    fix: update deployment script to handle gunicorn PID safely and remove logging redirection

--- start.sh
+++ start.sh
@@ -5,7 +5,9 @@ bun install
 bun run build
 cd ..
 cd backend
-xargs kill < /var/run/gunicorn.pid
+if [ -f /var/run/gunicorn.pid ]; then
+  xargs kill < /var/run/gunicorn.pid
+fi
 uv sync
 uv pip install -e .
 uv run python -m gunicorn src.thesocialnetwork:app -b unix:/var/run/gunicorn.sock --pid /var/run/gunicorn.pid > /dev/null 2>&1 & disown
